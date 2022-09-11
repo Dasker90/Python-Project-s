11d0b6e996be5ab37b2de6299df85aa67e3947af
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-09-11 12:11:07 +0200

    Update Check Disk space.py

--- Check Disk space.py
+++ Check Disk space.py
@@ -6,7 +6,7 @@ print("| A.H.E Informatyka III rok|")
 print("| VI Semestr | Nr:142873   |")
 print("|--------------------------|")
 time.sleep(1)
-#------------------------------------------------
+#-------------------------------------------------------------
 diskSize = 100
 diskSizeUsed = 90
 fileSize = 15
@@ -16,6 +16,7 @@ if fileSize <= (diskSize - diskSizeUsed):
 else:
     print("No space on disk")
 time.sleep(1)
+#-------------------------------------------------------------
 print("|--------------------------|")
 input("Aby zakonczyc nacisnij klaiwsz enter:")
 time.sleep(3)
